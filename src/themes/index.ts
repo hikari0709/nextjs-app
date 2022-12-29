3f6ddbad9227b73d6fb5188dc214c24ebcbdb498
hikari <tanimoto0709@gmail.com>
2022-12-29 09:42:12 +0900

    update: 設定ファイル等

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './colors';
+import fontSizes from './fontSizes';
+import letterSpacings from './letterSpacings';
+import lineHeight from './lineHeight';
+import space from './space';
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeight,
+  colors
+} as const;
---
+++ src/themes/lineHeight.ts
@@ -0,0 +1,3 @@
+const lineHeights: string[] = ['1.4', '1.8', '2.2', '2.8', '3.2'];
+
+export default lineHeights;
